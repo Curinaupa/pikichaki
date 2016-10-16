9bd73c8499ffb165d4a19b01797c023e589a84f5
Curinaupa <curinaupa@gmail.com>
2016-10-16 18:16:31 +0300

    Update Lab61.java

--- lab6/src/main/java/com/brainacad/Lab61.java
+++ lab6/src/main/java/com/brainacad/Lab61.java
@@ -3,11 +3,17 @@ import java.util.Arrays;
 
 //Create an array of all the even numbers from 2 to 30 (2,4,6...30) and display elements of the array to the screen.
 public class Lab61 {
-    public static void main(String[] args) {
-        int[] array1 = new int[15];
-        for (int i = 1; i <15 ; i++) {
-            array1[i-1] = i*2;
+
+    public void getArray(int [] array){
+        for (int i = 1; i <=15 ; i++) {
+            array[i-1] = i*2;
         }
-        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array));
+
+    }
+    public static void main(String[] args) {
+        Lab61 lab61 = new Lab61();
+        lab61.getArray(new int[15]);
+
     }
 }
