03b4ec7b0f9458dd39c67d0689f98a520e6f5553
Curinaupa <curinaupa@gmail.com>
2016-10-16 18:30:00 +0300

    Update Lab62.java

--- lab6/src/main/java/com/brainacad/Lab62.java
+++ lab6/src/main/java/com/brainacad/Lab62.java
@@ -11,13 +11,12 @@ import java.util.Arrays;
 //        Write code which calculates the maximum value from the array, the minimum value, the average and the median.
 
 public class Lab62 {
-    public static void main(String[] args) {
-        int[] m = new int[] { 10, 21, 5, 22, 9, 29, 25, 22, 11, 14, 8, 14 };
+
+    public void getMaxMin(int[] m){
         int maxValue = m[0];
         int minValue = m[0];
         int sum = 0;
         double average;
-        int mediana;
         for (int i = 0; i < m.length; i++) {
             sum += m[i];
             if (maxValue < m[i]){
@@ -29,18 +28,23 @@ public class Lab62 {
         }
         average =(double) sum/m.length;
         System.out.println(Arrays.toString(m));
-        System.out.println("Max Value = " + maxValue);
-        System.out.println("Min Value = " + minValue);
-        System.out.println("Averange = " + average);
-//        Arrays.sort(m);
-//        System.out.println(Arrays.toString(m));
-        if(m.length%2==0){
-            mediana = (m[m.length/2-1]+m[m.length/2])/2;
-        } else {
-            mediana = m[m.length/2];
-        }
-        System.out.println("Mediana = " + mediana );
-        // Arrays.binarySearch(m,);
+        getMessage(maxValue);
+        getMessage(minValue);
+        getMessage(average);
+//        System.out.println("Max Value = " + maxValue);
+//        System.out.println("Min Value = " + minValue);
+//        System.out.println("Averange = " + average);
+    }
+    public void getMessage(int s){
+        System.out.println( s );
+    }
+    public void getMessage(double s){
+        System.out.println( s );
+    }
+    public static void main(String[] args) {
+        int [] n = new int[] { 10, 21, 5, 22, 9, 29, 25, 22, 11, 14, 8, 14 };
+        Lab62 lab62 = new Lab62();
+        lab62.getMaxMin(n);
 
 
     }
