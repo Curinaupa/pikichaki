471ee1248b437655aad6ec6a706c45f7c7b06e82
Huamani V.R <curinaupa@gmail.com>
2016-10-22 21:48:54 +0300

    Update1

---
+++ lab6/src/main/java/com/brainacad/Lab224.java
@@ -0,0 +1,20 @@
+package com.brainacad;
+
+/**
+ * Create class Person with fields firstName(String),
+ * lastName(String), age(int), gender(String), phoneNumber(int),
+ * and  five overloaded methods that set this fields with different
+ * arguments list. And create class that will be use instance of class
+ * Person and his methods.
+ */
+class Persone{
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String gender;
+    private int phoneNumber;
+
+
+}
+public class Lab224 {
+}
